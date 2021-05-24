691077e7590bf6cc66c16089adf3c3722cb8a217
Joudrier Kevin <kev.joudrier@gmail.com>
2021-05-24 16:04:51 +0200

    Création du bouton pour dépoloyer la sidebar et reglage de grille pour le responsive.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,23 @@
 (function() {
     const button_inforamtion = document.getElementById('js-button-infomration');
     const informations_element = document.getElementById('js-information-bloc');
-    
+    const button_sidebar = document.getElementById('js-button-navigation');
+    const sidebar_element = document.getElementById('js-sidebar-element');
+    const content_element = document.getElementById('js-content-element');
+
     const onClickButtonInformation = (event) => {
         event.preventDefault();
         informations_element.classList.toggle('visible');
     }
 
-    button_inforamtion.addEventListener('click', onClickButtonInformation);
+    const onClickButtonSidebar = (event) => {
+        event.preventDefault();
 
+        button_sidebar.parentNode.classList.toggle('visible');
+        sidebar_element.classList.toggle('visible');
+        content_element.classList.toggle('visible');
+    }
+
+    button_inforamtion.addEventListener('click', onClickButtonInformation);
+    button_sidebar.addEventListener('click', onClickButtonSidebar);
 })()
